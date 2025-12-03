4fe582b3e443f1d8149dca5cb1a4b457bc24d7d9
Ingrid Marguerite Wagnon <maggy.wagnon@googlemail.com>
2025-12-03 10:29:48 +0100

    18.9

--- R/functions.R
+++ R/functions.R
@@ -8,13 +8,15 @@
 create_table_descriptive_stats <- function(data) {
   tablestat <- data |>
     dplyr::group_by(metabolite) |>
-    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd, median = median, iqr = IQR))) |>
     dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) round(x, digits = 1))) |>
-    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
-    dplyr::select(Metabolite = metabolite, `Mean SD` = MeanSD)
+    dplyr::mutate(
+      MeanSD = glue::glue("{value_mean} ({value_sd})"),
+      MedianIQR = glue::glue("{value_median} ({value_iqr})")
+    ) |>
+    dplyr::select(Metabolite = metabolite, `Mean SD` = MeanSD, `Median IQR` = MedianIQR)
   return(tablestat)
 }
-
 
 
 #' Plot distribution metabolites value
@@ -25,7 +27,7 @@ create_table_descriptive_stats <- function(data) {
 #'
 create_plot_distributions <- function(data) {
   plotdistrib <- data |>
-  ggplot2::ggplot(ggplot2::aes(x = value)) +
+    ggplot2::ggplot(ggplot2::aes(x = value)) +
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free") +
     ggplot2::theme_minimal()
