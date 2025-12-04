dd646674d737787e56cb8ffb65dffd0a8b6fcc82
Ingrid Marguerite Wagnon <maggy.wagnon@googlemail.com>
2025-12-04 10:00:48 +0100

    22.4

--- R/functions.R
+++ R/functions.R
@@ -120,3 +120,25 @@ fit_all_models <- function(data) {
     purrr::map(\(model) fit_model(data, model = model)) |>
     purrr::list_rbind()
 }
+
+
+#' Plot the estimates and standard errors of the model results.
+#'
+#' @param results The model results.
+#'
+#' @return A ggplot2 figure.
+#'
+create_plot_model_results <- function(results) {
+  results |>
+    dplyr::filter(term == "value", std.error <= 2, estimate <= 5) |>
+    dplyr::select(metabolite, model, estimate, std.error) |>
+    ggplot2::ggplot(ggplot2::aes(
+      x = estimate,
+      y = metabolite,
+      xmin = estimate - std.error,
+      xmax = estimate + std.error
+    )) +
+    ggplot2::geom_pointrange() +
+    ggplot2::geom_vline(xintercept = 1, linetype = "dashed") +
+    ggplot2::facet_grid(cols = ggplot2::vars(model))
+}
